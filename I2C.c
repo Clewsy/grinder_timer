260d7f5a9aea62bdb337e054e7d3537b0958198a
Clewsy <jadon.clews@gmail.com>
2017-11-14 20:59:13 +1100

    Added SDD1306 driver with init and addressing functionality.

--- I2C.c
+++ I2C.c
@@ -1,17 +1,19 @@
 #include "I2C.h"
 
+//Initialise the I2C hardware.
 void i2c_init(void)
 {
-	TWBR = 2;		//Gives SCL Frequency=400000Hz if F_CPU is 8MHz
-				//TWBR: TWI Bit-Rate Register - Used to det the TWI (I2C) Frequency (SCL)
-				//SCL frequency = (CPU Freq)/( 16 + 2(TWBR)*(PrescalerValue) )
-				//Note, PrescalerValue default = 1, otherwise set in TWSR with bits TWPS[1:0]
-				//TWSR: TWI Status Register
+	TWBR = 2;	//Gives SCL Frequency=400000Hz if F_CPU is 8MHz
+			//TWBR: TWI Bit-Rate Register - Used to det the TWI (I2C) Frequency (SCL)
+			//SCL frequency = (CPU Freq)/( 16 + 2(TWBR)*(PrescalerValue) )
+			//Note, PrescalerValue default = 1, otherwise set in TWSR with bits TWPS[1:0]
+			//TWSR: TWI Status Register
 
 	TWCR |= (1 << TWEN);	//TWCR: TWI Control Register
 				//TWEN: TWI Enable Bit - turns on TWI (I2C) hardware.
 }
 
+//Loop until the TWI (I2C) interrupt flag is set indicating an operation has just completed.
 void i2c_wait_for_complete(void)
 {
 	while(!(TWCR & (1 << TWINT))) {}	//Loop until current TWI (I2C) operation is complete (bit is set).
@@ -19,27 +21,28 @@ void i2c_wait_for_complete(void)
 						//Note, TWINT must be cleared by software - this indicates start of next operation.
 						//Extra note, reading TWINT gives status of flag (1=set, 0=clear) but to clear
 						//flag, software must write 1 to TWINT.
-
-
 }
 
+//Set the I2C start condition.
 void i2c_start(void)
 {
-	TWCR = ((1 << TWEA) | (1 << TWEN) | (1 << TWSTA) | (1 << TWINT));	//A TWI start is initiated by setting the following bits on TWCR
+	TWCR = ((1 << TWEN) | (1 << TWSTA) | (1 << TWINT));	//A TWI start is initiated by setting the following bits on TWCR
 								//TWEN - TWI enable
 								//TWSTA - TWI Start Condition
 								//TWINT - Clear TWINT flag (will be set at completion of operation)
-	i2c_wait_for_complete();		//Wait until previous operation is complete.
+	i2c_wait_for_complete();	//Wait until previous operation is complete.
 }
 
+//Set the I2C stop condition.
 void i2c_stop(void)
 {
-	TWCR = ((1 << TWEA) | (1 << TWEN) | (1 << TWSTO) | (1 << TWINT));	//A TWI start is initiated by setting the following bits on TWCR
+	TWCR = ((1 << TWEN) | (1 << TWSTO) | (1 << TWINT));	//A TWI start is initiated by setting the following bits on TWCR
 								//TWEN - TWI enable
 								//TWSTO - TWI Stop Condition
 								//TWINT - Clear TWINT flag (will be set at completion of operation)
 }
 
+//Transmit a byte.
 void i2c_transmit(uint8_t byte)
 {
 	TWDR = byte;				//TWDR: TWI (I2C) Data Register
--- SDD1306.c
+++ SDD1306.c
@@ -6,24 +6,73 @@
 //Send the control byte that signals the next byte is either a command (0x80) or data (0x40).
 //Send either the actual control byte or the data byte/s.
 
-//#define CONTROL_BYTE_COMMAND	0x80
-//#define CONTROL_BYTE_DATA	0x40
-
-void oled_send_command (uint8_t command)
+//Using the I2C hardware, send a command packet.  This must include the control byte that indicates a command.
+void oled_send_command(uint8_t command)
 {
-	i2c_start();
-	i2c_transmit(OLED_WRITE);
-	i2c_transmit(CONTROL_BYTE_COMMAND);
-	i2c_transmit(command);
-	i2c_stop();
+	i2c_start();				//Set I2C start condition
+	i2c_transmit(OLED_WRITE);		//Transmit slave address & write bit.
+	i2c_transmit(CONTROL_BYTE_COMMAND);	//Transmit control byte to indicate a command.
+	i2c_transmit(command);			//Transmit the command byte.
+	i2c_stop();				//Set the I2C stop condition.
 }
 
-void oled_send_data (uint8_t data)
+//Using the I2C hardware, send a data packet.  This must include the control byte that indicates data.
+void oled_send_data(uint8_t data)
 {
-	i2c_start();
-	i2c_transmit(OLED_WRITE);
-	i2c_transmit(CONTROL_BYTE_DATA);
-	i2c_transmit(data);
-	i2c_stop();
+	i2c_start();				//Set I2C start condition.
+	i2c_transmit(OLED_WRITE);		//Transmit slave address & write bit.
+	i2c_transmit(CONTROL_BYTE_DATA);	//Transmit control byte to indicate data.
+	i2c_transmit(data);			//Transmit the data.
+	i2c_stop();				//Set the I2C stop condition.
 }
 
+//Initialise the SDD1306 OLED driver.  This listing follows the suggested initialisation flow from the datasheet.
+//A lot of these commands are the defaults anyway, but it doesn't hurt to include them.
+//Refer to the .h file for descriptions or the datasheet for detail.
+void oled_init(void)
+{
+	oled_send_command(SET_DISPLAY_OFF);
+	oled_send_command(SET_MULTIPLEX_RATIO);
+	oled_send_command(MULTIPLEX_RATIO_64);
+	oled_send_command(SET_DISPLAY_OFFSET);
+	oled_send_command(DISPLAY_OFFSET_0);
+	oled_send_command(SET_DISPLAY_START_LINE_0);	
+	oled_send_command(SET_SEGMENT_REMAP_0);
+	oled_send_command(SET_COM_OUTPUT_SCAN_DIR_NORMAL);
+	oled_send_command(SET_COM_PINS_HARDWARE_CONFIG);
+	oled_send_command(COM_PINS_HARDWARE_DEFAULT);
+	oled_send_command(SET_DISPLAY_CONTRAST);
+	oled_send_command(CONTRAST_HIGH);
+	oled_send_command(RESUME_FROM_ENTIRE_DISPLAY_ON);
+	oled_send_command(SET_DISPLAY_NOT_INVERSE);
+	oled_send_command(SET_DISPLAY_CLOCK_DIVIDE);
+	oled_send_command(CLOCK_DIVIDE_DEFAULT);
+	oled_send_command(CHARGE_PUMP_CONTROL);
+	oled_send_command(ENABLE_CHARGE_PUMP);
+	oled_send_command(SET_DISPLAY_ON);
+}
+
+//This function sets the address from which oled image data will be written.
+//Note, in page address mode, the column is automatically incremented with every data byte writte, but the page must be set manually.
+//Consider the column to be the x co-ords (0to127) and the page to be the y co-ords (0 to 7).
+//Each data byte is represented as a vertical line of 8 pixels. 
+void oled_set_address(uint8_t column, uint8_t page)
+{
+	oled_send_command(SET_LOW_COLUMN_START_ADDRESS | (column & 0x0F));	//Send the byte that sets the column address lower nibble.
+	oled_send_command(SET_HIGH_COLUMN_START_ADDRESS | (column >> 4));	//Send the byte that sets the column address higher nibble.
+	oled_send_command(SET_PAGE_START_ADDRESS | page);			//Send the byte that sets the page address.
+}
+
+//Set every data bit to zero in order to clear the screen.
+void oled_clear_screen(void)
+{
+	uint8_t i,j;
+	for(i=0;i<8;i++)			//i cycles from 0 to 7 to write to each page.
+	{
+		oled_set_address(0,i);		//Only the page needs to be addressed manually as the column increments with every data byte sent.
+		for(j=0;j<128;j++)		//j cycles from 0 to 127 for each column.
+		{
+			oled_send_data(0x00);	//Send all 0s to clear the pixels.
+		}
+	}
+}
